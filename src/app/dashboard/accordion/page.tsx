1ab2688c81bec0cb0e8acd8be49f183b6ca9ad0d
gustavo <gustavo.perez.231191@gmail.com>
2024-05-30 01:10:02 -0600

    Added button, accordion and alert examples

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+
+const items = [
+    {
+        id: 'item-1',
+        question: 'Is it accessible?',
+        answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+    },
+    {
+        id: 'item-2',
+        question: 'Is it styled?',
+        answer: 'Yes. It comes with default styles that matches the other components aesthetic.',
+    },
+    {
+        id: 'item-3',
+        question: 'Is it animated?',
+        answer: 'Yes. Its animated by default, but you can disable it if you prefer.',
+    },
+    {
+        id: 'item-4',
+        question: 'Is it true?',
+        answer: 'Yes. Its true by default, but you can it if you prefer.',
+    },
+];
 
 export default function page() {
-    return <div>Accordion Page</div>;
+    return (
+        <Accordion type="single" className="w-full">
+            {items.map((item) => (
+                <AccordionItem key={item.id} value={item.id}>
+                    <AccordionTrigger>{item.question}</AccordionTrigger>
+                    <AccordionContent>{item.answer}</AccordionContent>
+                </AccordionItem>
+            ))}
+        </Accordion>
+    );
 }
--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { RocketIcon } from '@radix-ui/react-icons';
 
 export default function page() {
-    return <div>Alert Page</div>;
+    return (
+        <div className="grid gap-3">
+            <Alert>
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+            <Alert variant="destructive">
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+            <Alert variant="success">
+                <RocketIcon className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can add components to your app using the cli.
+                </AlertDescription>
+            </Alert>
+        </div>
+    );
 }
